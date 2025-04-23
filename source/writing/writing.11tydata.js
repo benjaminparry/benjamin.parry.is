369bf526a3a9fe9f8c9cea98d311f5a0a104deaa
benjaminparry <benjamin@parry.is>
2025-04-23 13:12:10 +0100

    Disable webmentions.

--- source/writing/writing.11tydata.js
+++ source/writing/writing.11tydata.js
@@ -1,10 +1,10 @@
-const { getWebmentions } = require("@chrisburnell/eleventy-cache-webmentions");
-const configWebmentions = require("../_data/configWebmentions.js");
+// const { getWebmentions } = require("@chrisburnell/eleventy-cache-webmentions");
+// const configWebmentions = require("../_data/configWebmentions.js");
 
-module.exports = {
-	eleventyComputed: {
-		webmentions: (data) => {
-			return getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
-		},
-	}
-}
+// module.exports = {
+// 	eleventyComputed: {
+// 		webmentions: (data) => {
+// 			return getWebmentions(configWebmentions, configWebmentions.domain + data.page.url)
+// 		},
+// 	}
+// }
